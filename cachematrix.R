222a668a14c656fabd06ce5347f89601eb06ad91
E. Futch <efutch@gmail.com>
2020-08-09 23:56:13 -0400

    Completed cacheSolve for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to create a cached matrix inversion function
+## Based on the README.md example of a ached mean
 
-## Write a short comment describing this function
 #The first function, `makeCacheMatrix` creates a special "matrix", which is
 #really a list containing a function to
 #
@@ -26,8 +25,18 @@ makeCacheMatrix <- function(x = matrix())
 }
 
 
-## Write a short comment describing this function
+## Return an inverse matrix, if it was calculated before return the cached version
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("Using cached results")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
